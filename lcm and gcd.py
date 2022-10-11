c0b23c991730880a0e4d20ad59e1ca3a6cf81ec7
yaswanth8978 <115204301+yaswanth8978@users.noreply.github.com>
2022-10-11 14:17:43 +0530

    Create lcm and gcd.py

---
+++ lcm and gcd.py
@@ -0,0 +1,13 @@
+num = int(input("enter the n values :"))
+n1 = int(input("Enter First number :"))
+n2 = int(input("Enter Second number :"))
+x = n1
+y = n2
+while(n2!=0):
+    t = n2
+    n2 = n1 % n2
+    n1 = t
+gcd = n1
+print("GCD of {0} and {1} = {2}".format(x,y,gcd))
+lcm = (x*y)/gcd
+print("LCM of {0} and {1} = {2}".format(x,y,lcm))
