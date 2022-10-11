023a73a7e8274d62845eb04563db667ee603369d
yaswanth8978 <115204301+yaswanth8978@users.noreply.github.com>
2022-10-11 14:25:03 +0530

    Create swap two numbers.py

---
+++ swap two numbers.py
@@ -0,0 +1,9 @@
+x = input("enter the number 1 :")
+y = input("enter the number 2 :")
+
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
