476c37114f8d640a50a54f24dee2a007c6a555a1
yaswanth8978 <115204301+yaswanth8978@users.noreply.github.com>
2022-10-11 13:37:54 +0530

    Create percentage pattern.py

---
+++ percentage pattern.py
@@ -0,0 +1,11 @@
+def pypart(n):
+    for i in range(0, n):
+    
+        for j in range(0, i+1):
+         
+            print("-1 ",end="")
+      
+        print("\r")
+ 
+n = int(input("enter number of rows"))
+pypart(n)
